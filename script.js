af449f4a383ac7f6bf385fe0e7b0959355dbf636
lalit1716 <lnigam27@gmail.com>
2024-04-23 19:13:21 +0530

    download btn animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector(".wrapper"),
     qrInput = wrapper.querySelector(".form input"),
     generateBtn = wrapper.querySelector(".form button"),
     qrImg = wrapper.querySelector(".qr-code img");
-    downBtn = wrapper.querySelector(".down-btn");
+downBtn = wrapper.querySelector(".down-btn");
 let preValue;
 
 generateBtn.addEventListener("click", () => {
@@ -10,25 +10,33 @@ generateBtn.addEventListener("click", () => {
     if (!qrValue || preValue === qrValue) return;
     preValue = qrValue;
     generateBtn.innerText = "Generating QR Code...";
-    
     qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
 
     });
+
+    const downBtnTime = () =>{
+        downBtn.classList.add('active')
+
+    }
+
+    setTimeout(downBtnTime, 650);
+
 });
 
-downBtn.addEventListener('click', ()=>{
-    downBtn.href= qrImg.src;
+downBtn.addEventListener('click', () => {
+    downBtn.href = qrImg.src;
 })
 
 qrInput.addEventListener("keyup", () => {
     if (!qrInput.value.trim()) {
         wrapper.classList.remove("active");
         preValue = "";
-        
+
     }
 
-    
+
 });
